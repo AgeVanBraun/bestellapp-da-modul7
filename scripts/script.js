8acb0d4842a662e9232bc208c526d0c59ce77fd0
Heiko <heikobraun@AgeVanBraun>
2025-08-04 23:15:14 +0200

    object and template integrated

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+console.table(myOffers.Pizzas)
+
+function init() {
+  renderOffers();
+}
+
+function renderOffers() {
+  let contentRef = document.getElementById('pizzas_content');
+  contentRef.innerHTML = '';
+
+  for (let indexOffers = 0; indexOffers < myOffers.Pizzas.length; indexOffers++) {
+    contentRef.innerHTML += getOffersTemplate(indexOffers);
+      }
+}
---
+++ scripts/template.js
@@ -0,0 +1,4 @@
+function getOffersTemplate(indexOffers){
+    return `<div>
+                <h1>Hello World</h1>`
+}
