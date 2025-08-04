58e820f65a535052fac8d0c2a2020e3e84480ced
Heiko <heikobraun@AgeVanBraun>
2025-08-04 20:02:17 +0200

    update data.js

---
+++ scripts/data.js
@@ -0,0 +1,82 @@
+let myOffers = {
+  "Pizzas": [
+    {
+      "name": "Pizza Diavalo",
+      "price": 12.99,
+      "description": "Pizza mit Salami, Tomaten und Peperoni",
+      "amount": 0,
+    },
+    {
+      "name": "Pizza Margherita",
+      "price": 9.99,
+      "description": "Klassische Pizza mit Tomatensauce und Käse",
+      "amount": 0,
+    },
+    {
+      "name": "Pizza Funghi",
+      "price": 10.99,
+      "description": "Pizza mit frischen Champignons und Käse",
+      "amount": 0,
+    },
+    {
+      "name": "Pizza Hawaii",
+      "price": 11.99,
+      "description": "Pizza mit Schinken und Ananas",
+      "amount": 0,
+    },
+  ],
+
+  "Salads": [
+    {
+      "name": "Griechischer Salat",
+      "price": 7.99,
+      "description": "Tomaten, Zwiebel, Käse und Oliven",
+      "amount": 0,
+    },
+    {
+      "name": "Caesar Salat",
+      "price": 8.50,
+      "description": "Salat mit Hähnchen, Croutons und Parmesan",
+      "amount": 0,
+    },
+    {
+      "name": "Bunter Gartensalat",
+      "price": 6.99,
+      "description": "Frische Blattsalate mit Gemüse",
+      "amount": 0,
+    },
+    {
+      "name": "Thunfischsalat",
+      "price": 8.99,
+      "description": "Salat mit Thunfisch, Ei und Zwiebeln",
+      "amount": 0,
+    },
+  ],
+
+  "Drinks": [
+    {
+      "name": "Cola",
+      "price": 2.50,
+      "description": "Limonade",
+      "amount": 0,
+    },
+    {
+      "name": "Mineralwasser",
+      "price": 1.80,
+      "description": "Spritziges Wasser",
+      "amount": 0,
+    },
+    {
+      "name": "Orangensaft",
+      "price": 2.20,
+      "description": "Fruchtiger Saft aus Orangen",
+      "amount": 0,
+    },
+    {
+      "name": "Apfelschorle",
+      "price": 2.00,
+      "description": "Apfelsaft mit Sprudel gemischt",
+      "amount": 0,
+    },
+  ]
+};
