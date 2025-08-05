c486fb7e0388af354c09ef5ef233173f8e0a83b5
Heiko <heikobraun@AgeVanBraun>
2025-08-05 09:40:39 +0200

    added function offers template

--- scripts/template.js
+++ scripts/template.js
@@ -1,4 +1,11 @@
-function getOffersTemplate(indexOffers){
-    return `<div>
-                <h1>Hello World</h1>`
+function getOffersTemplate(indexOffers) {
+  return `<div class="pizza-box">
+
+            <button id="box_btn" class="box-btn">+</button>
+                <div class="pizza-content">
+                    <h1>${myOffers.Pizzas[indexOffers].name}</h1>
+                    <span>${myOffers.Pizzas[indexOffers].description}</span>
+                    <p>${myOffers.Pizzas[indexOffers].price.toFixed(2).replace('.',',') + ' €'}</p>
+                </div>
+            </div>`;
 }
